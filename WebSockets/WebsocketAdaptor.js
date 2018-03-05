c9fcee41eaf9d84f0af4d994dad484a53e496c68
inmysocks <inmysocks@gmail.com>
2018-03-05 11:53:21 +0100

    maybe this will fix the problem where some tiddlers are getting double prefixes

--- WebSockets/WebsocketAdaptor.js
+++ WebSockets/WebsocketAdaptor.js
@@ -180,7 +180,7 @@ WebsocketAdaptor.prototype.saveTiddler = function(tiddler, prefix, callback) {
     }
   }
   prefix = prefix || '';
-  var internalName = prefix === '' ? tiddler.fields.title:`{${prefix}}${tiddler.fields.title}`;
+  var internalName = (prefix === '' && !tiddler.fields.title.startsWith(`{${prefix}}`)) ? tiddler.fields.title:`{${prefix}}${tiddler.fields.title}`;
   if (tiddler && $tw.MultiUser.ExcludeList.indexOf(tiddler.fields.title) === -1 && !tiddler.fields.title.startsWith('$:/state/') && !tiddler.fields.title.startsWith('$:/temp/')) {
     var self = this;
     var tempTiddlerFields = {};
