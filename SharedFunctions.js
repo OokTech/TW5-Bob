6531acd6d8a96e41d87750e5fc3fc84febccc8ad
inmysocks <inmysocks@fastmail.com>
2018-07-16 15:51:36 +0200

    Fix an error caused by the previous fix for an error caused by another fix for yet another error.

--- SharedFunctions.js
+++ SharedFunctions.js
@@ -270,8 +270,8 @@ This has some functions that are needed by Bob in different places.
     $tw.Bob.Timers = $tw.Bob.Timers || {};
     // Empty tags fields will be converted to empty strings.
     if (messageData.message.type === 'saveTiddler') {
-      if (!Array.isArray(messageData.message.tiddler.fields.tags) && messageData.message.tiddler.fields.tags === '') {
-        messageData.message.tiddler.fields.tags = [];
+      if (!Array.isArray(messageData.message.tiddler.fields.tags)) {
+        messageData.message.tiddler.fields.tags = $tw.utils.parseStringArray(messageData.message.tiddler.fields.tags);
       }
     }
     connectionIndex = connectionIndex || 0;
