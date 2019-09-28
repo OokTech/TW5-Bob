f884c825edd8ddecf48b01e1dc5108eeaf42bbf4
inmysocks <inmysocks@gmail.com>
2019-09-28 16:44:03 +0200

    magic?

--- Federation/FederationWebsocketSetup.js
+++ Federation/FederationWebsocketSetup.js
@@ -18,7 +18,7 @@ exports.after = ["render"];
 exports.synchronous = true;
 
 if($tw.node && $tw.settings.enableFederation === 'yes') {
-
+  console.log('here?')
   const setup = function () {
     $tw.Bob = $tw.Bob || {};
     $tw.nodeMessageHandlers = $tw.nodeMessageHandlers || {};
@@ -73,8 +73,10 @@ if($tw.node && $tw.settings.enableFederation === 'yes') {
       Setup the websocket server if we aren't using an external one
     */
     function finishSetup () {
+      console.log('finish setup')
       $tw.settings['fed-wss'] = $tw.settings['fed-wss'] || {};
       if(!$tw.settings['fed-wss'].useExternalWSS) {
+        console.log('here')
         $tw.federationWss = new WebSocketServer({noServer: true});
         // Set the onconnection function
         $tw.federationWss.on('connection', handleConnection);
@@ -83,6 +85,7 @@ if($tw.node && $tw.settings.enableFederation === 'yes') {
         $tw.federationWss.on('close', function(connection) {
           $tw.Bob.logger.log('closed remote connection ', connection, {level:2});
         });
+        console.log($tw.federationWss)
       }
     }
 
@@ -133,6 +136,7 @@ if($tw.node && $tw.settings.enableFederation === 'yes') {
   // we have to use the command line arguments because the externalserver
   // command hasn't run yet so we can't check $tw.ExternalServer
   if($tw.boot.argv.indexOf('--externalserver') === -1) {
+    console.log('setup first')
     setup();
   }
 }
