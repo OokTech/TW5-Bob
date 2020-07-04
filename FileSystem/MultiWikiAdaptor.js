8737c1dd66704042cf5c6569f3d4cead445f4d6f
inmysocks@fastmail.com <inmysocks@fastmail.com>
2020-07-04 14:59:38 +0200

    fix where internal save is called

--- FileSystem/MultiWikiAdaptor.js
+++ FileSystem/MultiWikiAdaptor.js
@@ -168,9 +168,6 @@ if($tw.node) {
     }
     function finish() {
       if (tiddler && $tw.Bob.Wikis[prefix].wiki.filterTiddlers($tw.Bob.ExcludeFilter).indexOf(tiddler.fields.title) === -1) {
-        // Save the tiddler in memory.
-        internalSave(tiddler, prefix);
-        $tw.Bob.logger.log('Save Tiddler ', tiddler.fields.title, {level:2});
         self.getTiddlerFileInfo(new $tw.Tiddler(tiddler.fields), prefix,
          function(err,fileInfo) {
           if(err) {
@@ -178,6 +175,9 @@ if($tw.node) {
           }
           // Make sure that the tiddler has actually changed before saving it
           if ($tw.Bob.Shared.TiddlerHasChanged(tiddler, $tw.Bob.Wikis[prefix].wiki.getTiddler(tiddler.fields.title))) {
+            // Save the tiddler in memory.
+            internalSave(tiddler, prefix);
+            $tw.Bob.logger.log('Save Tiddler ', tiddler.fields.title, {level:2});
             try {
               $tw.utils.saveTiddlerToFileSync(new $tw.Tiddler(tiddler.fields), fileInfo)
               $tw.hooks.invokeHook('wiki-modified', prefix);
